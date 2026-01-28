950014b2e5d21f6eb0715c4bb460bbcb6d379257
Albert <137798889+IRIDIUM86@users.noreply.github.com>
2026-01-28 16:35:29 +0800

    Fixed directory bug

--- 50percentwork/src/main/java/com/random/percentwork/ConnectPython.java
+++ 50percentwork/src/main/java/com/random/percentwork/ConnectPython.java
@@ -1,7 +1,37 @@
 package com.random.percentwork;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
 public class ConnectPython {
-    public void connect(){
-        System.out.println("Button Works Fine");
+
+    public void connect() {
+        try {
+            ProcessBuilder pd = new ProcessBuilder("python", "ToDoList-main\\50percentwork\\scripts\\percent50.py");
+            Process process = pd.start();
+
+            // Read Python standard output
+            BufferedReader reader =
+                new BufferedReader(new InputStreamReader(process.getInputStream()));
+
+            String line;
+            while ((line = reader.readLine()) != null) {
+                System.out.println("PYTHON -> " + line);
+            }
+
+            // Read Python error output
+            BufferedReader errorReader =
+                new BufferedReader(new InputStreamReader(process.getErrorStream()));
+
+            while ((line = errorReader.readLine()) != null) {
+                System.err.println("PYTHON ERROR -> " + line);
+            }
+
+            process.waitFor();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
+
