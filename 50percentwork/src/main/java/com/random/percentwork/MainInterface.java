8d08c249b674c4e6ee51a6a5e2d08378cd5c619e
Albert <137798889+IRIDIUM86@users.noreply.github.com>
2026-01-29 18:38:00 +0800

    now it uses 3 columns

--- 50percentwork/src/main/java/com/random/percentwork/MainInterface.java
+++ 50percentwork/src/main/java/com/random/percentwork/MainInterface.java
@@ -4,6 +4,8 @@ package com.random.percentwork;
 import java.awt.BorderLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.File;
+
 import javax.swing.BoxLayout;
 import javax.swing.JButton;
 import javax.swing.JFrame;
@@ -19,13 +21,26 @@ public class MainInterface extends JFrame {
         // (For now close it first, i'm trying to make it can run on the background)
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         
+        // Ensure us using BorderLayout
+        this.setLayout(new BorderLayout());
+
+        // Frame size
+        this.setSize(1000,500);
+
+        // Method call
+        drawMiddleColumn();
+        drawRightColumn();
+        drawLeftColumn();
+    }
+
+    private void drawMiddleColumn(){
         // Component declaration
         JLabel test = new JLabel("This page is working");
         JButton testButton = new JButton("Test python connectivity");
         
         // Loading the panel
         JPanel panel = new JPanel();
-        
+
         // Sets the layout into boxlayout that basically put the components vertically
         panel.setLayout(new BoxLayout(panel,BoxLayout.Y_AXIS));
         
@@ -34,8 +49,7 @@ public class MainInterface extends JFrame {
         test.setAlignmentX(test.CENTER_ALIGNMENT);  // Align it to the center
         panel.add(testButton);
         testButton.setAlignmentX(testButton.CENTER_ALIGNMENT); // Allignt it to the center
-        this.getContentPane().add(panel, BorderLayout.CENTER);
-        this.setSize(500,400);
+        this.add(panel,BorderLayout.CENTER);
         
         testButton.addActionListener(new ActionListener(){
             @Override
@@ -45,5 +59,18 @@ public class MainInterface extends JFrame {
                 connect.connect();
             }
         });
+    }
+
+    private void drawRightColumn(){
+        ChatPanel chatpanel = new ChatPanel();
+
+        // Put this column at the rightest part of the frame.
+        this.add(chatpanel, BorderLayout.EAST);
+    }
+
+    private void drawLeftColumn(){
+        FilePanel filePanel = new FilePanel();
+
+        this.add(filePanel, BorderLayout.WEST);
     }
 }
