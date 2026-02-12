a9f3d1d677ed7f637705dc16a87b899f7365d71e
Albert <137798889+IRIDIUM86@users.noreply.github.com>
2026-02-12 08:51:17 +0800

    trying to pull from main

--- 50percentwork/src/main/java/com/random/percentwork/MainInterface.java
+++ 50percentwork/src/main/java/com/random/percentwork/MainInterface.java
@@ -4,6 +4,7 @@ package com.random.percentwork;
 import java.awt.BorderLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.File;
 
 import javax.swing.BoxLayout;
 import javax.swing.JButton;
@@ -33,31 +34,10 @@ public class MainInterface extends JFrame {
     }
 
     private void drawMiddleColumn(){
-        // Component declaration
-        JLabel test = new JLabel("This page is working");
-        JButton testButton = new JButton("Test python connectivity");
-        
-        // Loading the panel
-        JPanel panel = new JPanel();
+        ToDoList todolist = new ToDoList();
 
-        // Sets the layout into boxlayout that basically put the components vertically
-        panel.setLayout(new BoxLayout(panel,BoxLayout.Y_AXIS));
-        
-        // Adding the component
-        panel.add(test);
-        test.setAlignmentX(test.CENTER_ALIGNMENT);  // Align it to the center
-        panel.add(testButton);
-        testButton.setAlignmentX(testButton.CENTER_ALIGNMENT); // Allignt it to the center
-        this.add(panel,BorderLayout.CENTER);
-        
-        testButton.addActionListener(new ActionListener(){
-            @Override
-            public void actionPerformed(ActionEvent e){
-                // When the button is clicked
-                ConnectPython connect = new ConnectPython();
-                connect.connect();
-            }
-        });
+        // Put this column at the center of the frame.
+        this.add(todolist, BorderLayout.CENTER);
     }
 
     private void drawRightColumn(){
